90d80e019d8d2379d30e1bab089eccf7c9ef58d4
ToledoLBC <lucastoledobc@gmail.com>
2024-12-30 13:15:49 -0300

    cordel

--- sites/anexos/game.js
+++ sites/anexos/game.js
@@ -3,15 +3,15 @@
 let botoes = document.getElementById('icones')
 botoes.innerHTML = `
         <button type="button" id="back" class="button">
-            <img src="/anexos/midias/icon-back.svg" id="iback" class="icon" alt="Voltar">
+            <img src="anexos/midias/icon-back.svg" id="iback" class="icon" alt="Voltar">
         </button>
         <button type="button" id="info" class="button" onclick="info()">
-            <img src="/anexos/midias/icon-info.svg" id="iinfo" class="icon" alt="Informação">
+            <img src="anexos/midias/icon-info.svg" id="iinfo" class="icon" alt="Informação">
         </button>
         <div class="warn" id="warn">
         </div>
         <button type="button" id="maxi" class="button">
-            <img src="/anexos/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
+            <img src="anexos/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
         </button>
 
 `;
@@ -42,7 +42,7 @@ function info() {
     if (warn.style.display != 'block') {
         warn.style.display = 'block';
         img.innerHTML = `
-        <img src="/anexos/midias/icon-close.svg" id="iinfo" class="icon" alt="Informação">
+        <img src="anexos/midias/icon-close.svg" id="iinfo" class="icon" alt="Informação">
         `;
     } else {
         warn.style.display = 'none';
@@ -57,15 +57,15 @@ function maxi() {
     let elem = document.documentElement;
     if (elem.requestFullscreen) {
         elem.requestFullscreen(), img.innerHTML = `
-        <img src="/anexos/midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
+        <img src="anexos/midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
         `;
       } else if (elem.webkitRequestFullscreen) { /* Safari */
         elem.webkitRequestFullscreen(), img.innerHTML = `
-        <img src="/anexos/midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
+        <img src="anexos/midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
         `;
       } else if (elem.msRequestFullscreen) { /* IE11 */
         elem.msRequestFullscreen(), img.innerHTML = `
-        <img src="/anexos/midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
+        <img src="anexos/midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
         `;
       }
     }
@@ -74,15 +74,15 @@ function mini() {
     let img = document.getElementById("maxi");
     if (document.exitFullscreen) {
         document.exitFullscreen(), img.innerHTML = `
-        <img src="/anexos/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
+        <img src="anexos/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
         `;
     } else if (document.webkitExitFullscreen) { /* Safari */
         document.webkitExitFullscreen(), img.innerHTML = `
-        <img src="/anexos/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
+        <img src="anexos/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
         `;
     } else if (document.msExitFullscreen) { /* IE11 */
         document.msExitFullscreen(), img.innerHTML = `
-        <img src="/anexos/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
+        <img src="anexos/midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
         `;
     }
     } 
@@ -91,6 +91,6 @@ function mini() {
 const head = document.getElementsByTagName("head")[0];
 head.innerHTML += `
     <meta name="description" content="Site de Física para ensino básico, ensino médio e ensino superior.">
-    <link rel="shortcut icon" href="/anexos/midias/CTRNF-WarpOrb.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="anexos/midias/CTRNF-WarpOrb.ico" type="image/x-icon">
 
     `;
